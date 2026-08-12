7b5f68964e6c5b35c72aae01b8e0effcb9b094f2
adefavour1 <113237051+adefavour1@users.noreply.github.com>
2026-08-12 12:40:13 +0100

    Update 2_Air_Compressor.py

--- pages/2_Air_Compressor.py
+++ pages/2_Air_Compressor.py
@@ -64,7 +64,7 @@ with st.sidebar:
     st.divider()
     st.markdown("### Navigation Controls")
     
-    st.page_link("Home.py", label="Home Overview", icon="🏠")
+    st.page_link("app.py", label="Home Overview", icon="🏠")
     st.page_link("pages/Executive_Dashboard.py", label="Executive Dashboard", icon="📈")
     st.page_link("pages/AHU_Monitoring.py", label="AHU Monitoring", icon="❄️")
     st.page_link("pages/Air_Compressor.py", label="Air Compressor", icon="🌀")
